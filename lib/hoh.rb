d0aba8ec91d3f1755d08810b976303cfc1cce638
Elizabeth Kim <elizabeth.kim93@gmail.com>
2019-12-07 06:19:02 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -54,13 +54,4 @@ Bio_life = {
   }
 }
 
-  #  Phylum
-  #  Class
-  #  Order
-  #  Family
-  #  Genus
-  #  Species
-  # So, if we have the "Kingdom" node we should be able to "tunnel" into the
-  # HoH all the way to Species!
-
 end
